7d1a764a6699ebe59bbd4aa955a42cbc6a6999ea
Connor Ostrander <costrander6@gatech.edu>
2026-06-17 22:49:22 -0400

    fix: remove /api/ from path

--- SOAP.WebService.API/Presentation/Controllers/WorkflowRunController.cs
+++ SOAP.WebService.API/Presentation/Controllers/WorkflowRunController.cs
@@ -21,6 +21,6 @@ public class WorkflowRunController(IWorkflowRunRepository workflowRunRepository)
 
         await workflowRunRepository.Create(workflowRunEntity);
         
-        return Created($"/api/workflow-run/{workflowRunEntity.Id}", WorkflowRunResponseMapper.MapEntityToResponse(workflowRunEntity));
+        return Created($"/workflow-run/{workflowRunEntity.Id}", WorkflowRunResponseMapper.MapEntityToResponse(workflowRunEntity));
     }
 }
